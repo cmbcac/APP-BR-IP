7fa4d245f782b67f08f7f78c10687154770f64b5
cmbcac <cmb.cac@gmail.com>
2019-03-28 11:32:20 +0100

    new index v2

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+var size;
+function adjust(){
+	if(window.innerWidth<=780 && window.innerWidth>320){
+		size="medium";
+		
+	}else if(window.innerWidth<=320){
+		size="small";
+		
+	}else{
+		size="large";
+		
+	}
+}	
